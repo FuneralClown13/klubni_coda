55863fb6d32c85d688251a85c4df176e995e6888
FuneralClown13 <funeralclown@yandex.ru>
2023-02-26 14:37:04 +0200

    fix tree.py

--- python_course/tree.py
+++ python_course/tree.py
@@ -14,12 +14,12 @@ def build_tree(level=0, print_pattern=(0,)) -> None:
     for i, file in enumerate(list_directory):
         last_file_flag = len(list_directory) - i - 1
         if os.path.isdir(f'{path}/{file}'):
-            printTreeLine(last_file_flag, print_pattern, file)
+            print_tree_line(last_file_flag, print_pattern, file)
             count_dir += 1
             os.chdir(f'{path}/{file}')
-            buildTree(level + 1, (*print_pattern, (1 if last_file_flag else 0)))
+            build_tree(level + 1, (*print_pattern, (1 if last_file_flag else 0)))
         else:
-            printTreeLine(last_file_flag, print_pattern, file)
+            print_tree_line(last_file_flag, print_pattern, file)
             count_files += 1
 
 
@@ -30,12 +30,12 @@ def print_tree_line(last_file: int, print_pattern: tuple, file_name: str) -> Non
 
 
 if __name__ == '__main__':
-    buildTree()
+    build_tree()
     print(f'{count_dir} directories, {count_files} files')
 
 
     def test(path: str):
         os.chdir(path)
-        buildTree()
+        build_tree()
 
     # test('/home/funeralclown/testtree')
