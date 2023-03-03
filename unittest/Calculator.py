1a90c5a1545bf72f96ee3d02cee0a43af3096d90
FuneralClown13 <funeralclown@yandex.ru>
2023-03-03 16:35:49 +0200

    fix Calculator.py

--- unittest/Calculator.py
+++ unittest/Calculator.py
@@ -1,6 +1,7 @@
 from re import findall
 from operator import truediv, mul, sub, add
 
+
 def calculate(expression: str):
     if not isinstance(expression, str):
         return 'Bad object type'
@@ -21,15 +22,3 @@ def calculate(expression: str):
             i = lst_line.index(key)
             lst_line[i - 1] = cmds[key](float(lst_line.pop(i - 1)), float(lst_line.pop(i)))
     return float(*lst_line)
-
-
-if __name__ == '__main__':
-    cases = (
-        ('123+123', 246),
-        (True, 'Bad object type')
-    )
-
-    for test in cases:
-        inp, equal = test
-        print(calculate(inp))
-        assert calculate(inp) == equal
